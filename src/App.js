cbbfebd7fab02ee20eb7e636250afc450fc6c30e
Álvaro Zájara <alzca1@gmail.com>
2021-10-26 13:16:30 +0200

    added more logic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import Typography from "@material-ui/core/Typography";
 
+const books = [{ name: "Refactoring" }, { name: "Domain-driven design" }];
+
 function App() {
+  const renderBooks = (books) => {
+    return (
+      <div data-test="book-list">
+        {books.map((book) => {
+          return (
+            <div className="book-item">
+              <h2 className="title">{book.name}</h2>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
   return (
     <div className="App">
       <Typography variant="h2" component="h2" data-test="heading">
         Bookishhhh
       </Typography>
-      <div data-test="book-list">
-        <div className="book-item"></div>
-        <div className="book-item"></div>
-      </div>
+      {renderBooks(books)}
     </div>
   );
 }
