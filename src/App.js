790c976893721e67f6e8a5f47ce2a7e4db010cfe
Álvaro Zájara <alzca1@gmail.com>
2021-10-26 12:03:27 +0200

    first changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { Typography } from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello World</h1>
+      <Typography variant="h2" component="h2" data-test="heading">
+        Bookishhhh
+      </Typography>
     </div>
   );
 }
