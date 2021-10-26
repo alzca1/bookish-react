e2c31b0d7217e8f2f44501ccc35bb00686341be0
Álvaro Zájara <alzca1@gmail.com>
2021-10-26 12:12:57 +0200

    wrote first test and fixed cypress eslint integration

---
+++ cypress/integration/ui/bookish.spec.js
@@ -0,0 +1,13 @@
+// bookish.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+describe("Bookish application", () => {
+  it("Visits the bookish", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('h2[data-test="heading"]').contains("Bookish");
+  });
+});
