c27c693f9232c1942f2480554c31d9b7a1e1460e
Álvaro Zájara <alzca1@gmail.com>
2021-10-27 18:49:10 +0200

    removed unnecessary logic from bookish spec

--- cypress/integration/ui/bookish.spec.js
+++ cypress/integration/ui/bookish.spec.js
@@ -7,31 +7,6 @@
 
 import axios from "axios";
 
-// before(() => {
-//   return axios
-//     .delete("http://localhost:8080/books?_cleanup=true")
-//     .catch((err) => err);
-// });
-
-// afterEach(() => {
-//   return axios
-//     .delete("http://localhost:8080/books?_cleanup=true")
-//     .catch((err) => err);
-// });
-
-// beforeEach(() => {
-//   const books = [
-//     { "name": "Refactoring", "id": 1 },
-//     { "name": "Domain-driven design", "id": 2 },
-//     { "name": "Building Microservices", "id": 3 },
-//   ];
-//   return books.map((item) =>
-//     axios.post("http://localhost:8080/books", item, {
-//       headers: { "Content-Type": "application/json" },
-//     })
-//   );
-// });
-
 describe("Bookish application", () => {
   it("Visits the bookish", () => {
     cy.visit("http://localhost:3000/");
@@ -42,13 +17,14 @@ describe("Bookish application", () => {
     cy.get('div[data-test="book-list"]').should("exist");
 
     cy.get("div.book-item").should((books) => {
-      expect(books).to.have.length(3);
+      expect(books).to.have.length(4);
 
       const titles = [...books].map((x) => x.querySelector("h2").innerHTML);
       expect(titles).to.deep.equal([
         "Refactoring",
         "Domain-driven design",
         "Building Microservices",
+        "Acceptance Test Driven Development with React"
       ]);
     });
   });
@@ -60,5 +36,12 @@ describe("Bookish application", () => {
     // la url debería incluir el fragmento "/books/1"
     cy.url().should("include", "/books/1");
     cy.get("h2.book-title").contains("Refactoring");
+  });
+  it("Searches for a title", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get("div.book-item").should("have.length", 4);
+    cy.get('[data-test="search"] input').type("design");
+    cy.get("div.book-item").should("have.length", 1);
+    cy.get("div.book-item").eq(0).contains("Domain-drive design");
   });
 });
